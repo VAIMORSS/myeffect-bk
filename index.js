96713d5a71d380c2d9976b8946dc0274921042d1
VAIMORSS <vaimorss2468@gmail.com>
2021-08-16 12:32:06 -0300

    🎉 INT COMMIT: may be working

--- index.js
+++ index.js
@@ -5,17 +5,18 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 
 const corsOptions = {
-  origin: '*',
+  origin: true,
   credentials: true,            //access-control-allow-credentials:true
   optionSuccessStatus: 200
 }
 
-app.use(cors(corsOptions));
 
 
 // configure the app to use bodyParser()
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json());
+
+app.use(cors(corsOptions));
 
 app.get('/api/v1', function (req, res) {
   res.status(200).send('API v1 running');
